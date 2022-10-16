94bc9ae47cc43b772d582c11340d48f9a4f5751a
muthu <muthupandianthangarajan@gmail.com>
2022-10-16 14:15:42 +0530

    complete examples

--- events/dags/hello_world_dag.py
+++ events/dags/hello_world_dag.py
@@ -5,7 +5,7 @@ from datetime import timedelta, datetime
 
 # initializing the default arguments
 default_args = {
-		'owner': 'Ranga',
+		'owner': 'Muthu',
 		'start_date': datetime(2022, 3, 4),
 		'retries': 3,
 		'retry_delay': timedelta(minutes=5)
@@ -15,7 +15,7 @@ default_args = {
 hello_world_dag = DAG('hello_world_dag',
 		default_args=default_args,
 		description='Hello World DAG',
-		schedule_interval='* * * * *', 
+		schedule_interval=None, 
 		catchup=False,
 		tags=['example, helloworld']
 )
@@ -25,13 +25,18 @@ def print_hello():
 	print("====")
 	return 'Hello World!'
 
+# python callable function
+def print_hello2():
+	print("====")
+	return 'Hello World!'
 
 # # Creating second task
 hello_world_task = PythonOperator(task_id='hello_world_task', python_callable=print_hello, dag=hello_world_dag)
+hello_world_task2 = PythonOperator(task_id='hello_world_task2', python_callable=print_hello2, dag=hello_world_dag)
 
 
 # # Set the order of execution of tasks. 
-hello_world_task
+hello_world_task >> hello_world_task2
 
 
 # with DAG("my_dag", start_date=datetime.now() as dag:
--- events/tasks.py
+++ events/tasks.py
@@ -1,6 +1,19 @@
 from celery import Celery
 from datetime import datetime
 from airflow import DAG
+from datetime import datetime, timedelta
+from requests.auth import HTTPBasicAuth
+import requests
+import json
+import base64
+from dateutil import tz
+from zoneinfo import ZoneInfo
+
+
+from_zone = tz.gettz('UTC')
+to_zone = tz.gettz('America/New_York')
+
+
 
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
@@ -12,5 +25,42 @@ def add(x, y):
     return x + y
 
 
+@app.task
+def af():
+    airflow_host = "localhost:8080"
+    airflow_username = "airflow"
+    airflow_password = "airflow"
+    start = datetime.now()
+    end = start + timedelta(days=1)
+    dag = "first_sample_dag"
+    days = (end -start).days
+    for i in range(days):
+        execution_datetime = (start + timedelta(hours=1)).strftime('%Y-%m-%dT%H:%M:%SZ')
 
+        url = f'http://{airflow_host}/api/v1/dags/{dag}/dagRuns'
+        up = f"{airflow_username}:{airflow_password}".encode("utf=8")
+        
+        headers = {
+            'Content-type': 'application/json', 
+            # 'Accept': 'application/json',
+            # 'Authorization': f"B {base64.b64encode(up)}"
+        }
+        data = {
+            'conf': {},
+            'dag_run_id': f'manual_api_{execution_datetime}',
+            'logical_date': execution_datetime,
+            'execution_date': execution_datetime
+        }
+        print(url)
+        result = requests.post(
+            url,
+            json=data,
+            headers=headers,
+            auth=(airflow_username, airflow_password)
+        )
+        result_json = result.json()
+        print(result_json)
+
+
+af()
 
